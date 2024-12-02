0cacb418f5af5c07e66b54421b4e0cf3f409685d
Grassboii <102272841+Grassboii@users.noreply.github.com>
2024-12-01 23:13:10 -0500

    Explosion Control 1.0.4

--- src/main/java/com/johnymuffin/beta/tntcontrol/ExplosionControlListener.java
+++ src/main/java/com/johnymuffin/beta/tntcontrol/ExplosionControlListener.java
@@ -1,6 +1,8 @@
 package com.johnymuffin.beta.tntcontrol;
 
 import java.util.ArrayList;
+
+import com.johnymuffin.beta.tntcontrol.config.ExplosionControlSettings;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -11,11 +13,36 @@ import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
+import static com.johnymuffin.beta.tntcontrol.ExplosionControl.blockBlacklist;
+import static com.johnymuffin.beta.tntcontrol.ExplosionControl.blockWhitelist;
+
 public class ExplosionControlListener implements Listener {
    private ExplosionControl plugin;
+   private long lastResetTime = System.currentTimeMillis();
+   private int explosionCount = 0;
+
+   private ExplosionControlSettings settings;
+
+   private final boolean tntChainingEnabled;
+   private final int tntChainingMaximum;
+   private final long tntChainingDurationMilliseconds;
+
+   private final boolean tntBehaviorCancelNonWhitelist;
+   private final boolean tntBehaviorUseTNTWhitelist;
+   private final boolean tntBehaviorIgnoreSingleBlockPopOff;
 
    public ExplosionControlListener(ExplosionControl plugin) {
       this.plugin = plugin;
+      settings = plugin.getSettings();
+
+      // Load Config Values
+      tntChainingEnabled = settings.getConfigBoolean("settings.tnt-chaining.enabled");
+      tntChainingMaximum = settings.getConfigInteger("settings.tnt-chaining.maximum");
+      tntChainingDurationMilliseconds = settings.getConfigLong("settings.tnt-chaining.duration-milliseconds");
+
+      tntBehaviorCancelNonWhitelist = settings.getConfigBoolean("settings.tnt-behavior.cancel-entire-explosion-if-non-whitelisted.enabled");
+      tntBehaviorUseTNTWhitelist = settings.getConfigBoolean("settings.tnt-behavior.use-tnt-whitelist.enabled");
+      tntBehaviorIgnoreSingleBlockPopOff = settings.getConfigBoolean("settings.tnt-behavior.ignore-single-if-block-pop-off.enabled");
    }
 
    @EventHandler
@@ -29,40 +56,73 @@ public class ExplosionControlListener implements Listener {
          event.setCancelled(true);
          return;
       }
-      ArrayList<Block> blocksToRemove = new ArrayList<>();
-      for (Block b : event.blockList()) {
-//         if (b.getLocation().getBlockY() >= 128) {
-//            blocksToRemove.add(b);
-//            continue;
-//         }
-         if (!this.plugin.getBlockWhitelist().contains(b.getType())){
-            blocksToRemove.add(b);
-            continue;
-         }
 
-         ArrayList<Material> blockCheckBlacklist = this.plugin.getBlockCheckBlacklist();
-         if(blockCheckBlacklist.contains(b.getRelative(0, 1, 0).getType())){
-            blocksToRemove.add(b);
-            continue;
-         }
-         if(blockCheckBlacklist.contains(b.getRelative(1, 0, 0).getType())){
-            blocksToRemove.add(b);
-            continue;
-         }
-         if(blockCheckBlacklist.contains(b.getRelative(-1, 0, 0).getType())){
-            blocksToRemove.add(b);
-            continue;
-         }
-         if(blockCheckBlacklist.contains(b.getRelative(0, 0, 1).getType())){
-            blocksToRemove.add(b);
-            continue;
-         }
-         if(blockCheckBlacklist.contains(b.getRelative(0, 0, 1).getType())){
-            blocksToRemove.add(b);
+      if(tntChainingEnabled){
+         long currentTime = System.currentTimeMillis();
+
+         if (currentTime - lastResetTime >= tntChainingDurationMilliseconds) {
+            lastResetTime = currentTime;
+            explosionCount = 0;
          }
       }
-      for (Block b : blocksToRemove)
-         event.blockList().remove(b);
+
+      ArrayList<Block> blocksToRemove = new ArrayList<>();
+      for (Block b : event.blockList()) {
+         Material type = b.getType();
+
+         if(tntBehaviorCancelNonWhitelist){
+            if (!blockWhitelist.contains(type)){
+               event.setCancelled(true);
+               return;
+            }
+         }
+         else if(tntBehaviorUseTNTWhitelist){
+            if (!blockWhitelist.contains(type)){
+               blocksToRemove.add(b);
+               continue;
+            }
+         }
+
+         if(tntBehaviorIgnoreSingleBlockPopOff) {
+            if(blockBlacklist.contains(b.getRelative(0, 1, 0).getType())){
+               blocksToRemove.add(b);
+               continue;
+            }
+            if(blockBlacklist.contains(b.getRelative(1, 0, 0).getType())){
+               blocksToRemove.add(b);
+               continue;
+            }
+            if(blockBlacklist.contains(b.getRelative(-1, 0, 0).getType())){
+               blocksToRemove.add(b);
+               continue;
+            }
+            if(blockBlacklist.contains(b.getRelative(0, 0, 1).getType())){
+               blocksToRemove.add(b);
+               continue;
+            }
+            if(blockBlacklist.contains(b.getRelative(0, 0, -1).getType())){
+               blocksToRemove.add(b);
+            }
+         }
+
+         if(tntChainingEnabled){
+            if(type == Material.TNT){
+               if (explosionCount >= tntChainingMaximum) {
+                  blocksToRemove.add(b);
+               }
+               else{
+                  explosionCount++;
+               }
+            }
+         }
+         else{
+            if(type == Material.TNT){
+               blocksToRemove.add(b);
+            }
+         }
+      }
+
+      event.blockList().removeAll(blocksToRemove);
    }
 
    @EventHandler
